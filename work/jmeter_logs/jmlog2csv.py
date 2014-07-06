af1e2d85dd110572407e35ecc30ffd71255a4a0e
Eugene Olkhovyk <eolizer@gmail.com>
2014-07-06 19:54:09 +0300

    jmlog2csv modified

--- work/jmeter_logs/jmlog2csv.py
+++ work/jmeter_logs/jmlog2csv.py
@@ -4,49 +4,74 @@ import os
 import sys
 import getopt
 
-def main(argv):
-	input_file = ""
-	output_iter = ""
-	output_summ = ""
+class jmlog2csv:
 
-	try:
-		opts, args = getopt.getopt(argv, "i:", ["input_file="])
-	except getopt.GetoptError:
-		print os.path.basename(__file__)+" -i[--input_file] <input file>"
-		sys.exit(2)
+	def __init__(self, argv):
+		self.argv = ""
+		
+		try:
+			opts, args = getopt.getopt(argv, "i:", ["input_file="])
+		except getopt.GetoptError:
+			print os.path.basename(__file__)+" -i[--input_file] <input file>"
+			sys.exit(2)
 	
-	for opt, arg in opts:
-		if opt in ("-i", "--input_file"):
-			input_file = arg
+		for opt, arg in opts:
+			if opt in ("-i", "--input_file"):
+				self.input_file_name = arg
 
-	output_iter = os.path.splitext(os.path.basename(input_file))[0]+"_iter.csv"
-	output_summ = os.path.splitext(os.path.basename(input_file))[0]+"_summ.csv"
-	
+		self.output_file_name = os.path.splitext(os.path.basename(self.input_file_name))[0]+".csv"
 
-	try:
-		inpf = open(input_file,"r")
-		outiterf = open(output_iter,"w")
-		outsummf = open(output_summ,"w")
-	except IOError, e:
-		print "I/O Error ({0}): {1}". format(e.errno, e.strerror)
-		sys.exit(2)
 
-	output_iter_hdr = "\"Phase requests\",\"Phase time\",\"Phase rate\",\"Phase avg. repl. time\",\"Phase max. repl. time\",\"Phase errors\",\"Phase error percentage\"\n"
-	output_summ_hdr = "\"Total requests\",\"Total time\",\"Total avg. rate\",\"Total avg. repl. time\",\"Total max. repl. time\",\"Total errors\",\"Total avg. error percentage\"\n"
-
-	outiterf.write(output_iter_hdr)
-	outsummf.write(output_summ_hdr)
-
-	for line in inpf:
-		line_list = line.split()
-		print line_list
-		if line_list[0] == "summary":
-			valsToWrite = [line_list[2], line_list[4][:-1], line_list[6][:-2], line_list[8], line_list[12], line_list[14], line_list[15].strip("(")[:-2]]
-			if line_list[1] == "+":
-				outiterf.write(",".join(valsToWrite)+"\n")
-			elif line_list[1] == "=":
-				outsummf.write(",".join(valsToWrite)+"\n")
+	def convert(self):
 				
+		try:
+			inpf = open(self.input_file_name,"r")
+			output_csv = open(self.output_file_name,"w")
+		except IOError, e:
+			print "I/O Error ({0}): {1}". format(e.errno, e.strerror)
+			sys.exit(2)
+
+
+
+		output_csv_hdr = "step_requests,total_requests,step_time,total_time,\
+step_avg_rate,total_avg_rate,step_avg_reply_time,\
+total_avg_reply_time,step_max_reply_time,\
+total_max_reply_time,step_errors,total_errors,\
+step_errors_percentage,total_error_percentage,\
+step_active_threads\n"
+	
+
+		output_csv.write(output_csv_hdr)
+
+		print "Processing file: "
+
+		i = 0
+		for line in inpf:
+			line_list = line.split()
+		
+			if line_list[0] == "summary":
+				if line_list[1] == "+":
+					valsToWrite = [line_list[2], "", line_list[4][:-1], "",\
+								 line_list[6][:-2], "", line_list[8], "",\
+								 line_list[12], "", line_list[14], "",\
+								 line_list[15].strip("(")[:-2], "", line_list[17]]
+				
+				elif line_list[1] == "=":
+					valsToWrite = ["", line_list[2], "", line_list[4][:-1], "",\
+								 line_list[6][:-2], "", line_list[8], "",\
+								 line_list[12], "", line_list[14], "",\
+								 line_list[15].strip("(")[:-2]]
+
+				i += 1 	
+				output_csv.write(",".join(valsToWrite)+"\n")
+				sys.stdout.write("#")
+		print "\nTotal lines processed: %d" % (i)
+		
+		return self.output_file_name
+
+
+
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+	jml = jmlog2csv(sys.argv[1:])
+	jml.convert()
